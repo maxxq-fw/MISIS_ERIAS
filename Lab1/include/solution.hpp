2eaf36bcfdd839f8592d474ca1e6fa5cd11ddaf0
maxxq <grishechkinm@gmail.com>
2026-09-25 00:07:42 +0300

    [Lab 1] Solution archeticture released

---
+++ Lab1/include/solution.hpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <utility>
+
+class Solution {
+private:
+    std::vector<std::vector<double>> _matrix;
+    std::pair<int, int> _closestProds;
+    std::pair<int, int> _closestUsers;
+
+private:
+    std::vector<double> getMatrixColumn(int column) const; 
+    std::vector<double> getMatrixRow(int row) const;
+    void setClosestProds(int firstRow, int secondRow);
+    void setClosestUsers(int firstColumn, int secondColumn);
+    double cosineSimilarity(const std::vector<double>& first, const std::vector<double>& second) const;
+    
+public:
+    Solution();
+    void matrixInit();
+    void calculateClosestProds();
+    void calculateClosestUsers();
+    std::pair<int, int> getClosestProds() const;
+    std::pair<int, int> getClosestUsers() const;
+};
